42ff739a3c3b73e4688ecdb3bb82841f7e50af2c
Shilpa Rao <shilpa.padiki.rao@gmail.com>
2021-04-26 16:18:38 -0700

    Update DAQplan.py

--- DAQplan.py
+++ DAQplan.py
@@ -1,32 +1,35 @@
-#!/usr/bin/env python3
-
-import pandas as pd
-import matplotlib as mat
-import serial as ser
-
-##DATA ACQUISITION:
-##    GPS coordinates
-##        - list view, last coordinate highlighted within several degrees of accuracy. continuous measurement; program stops collection after GPS coordinates have been stable for a certain period of time.
-##    Accelerometer
-##        - versus time
-##    Altimeter (BMP390L)
-##    Temperature (IMU)
-##    Angular velocity (IMU)
-##    Barometric pressure (BMP390L)
+import matplotlib.pyplot as plt
+from time import sleep
+import serial
 
 
-## pandas initial test
-## a really quick plot of velocity versus time from testData.csv
+pressure = [];
+ser = serial.Serial('(WHAT SERIAL PORT):/dev/tty.usbmodem1d11', 9600) # Establish the connection on a specific port
+counter = 32 # Below 32 everything in ASCII is gibberish
+while True:
+     counter +=1
+     currPress = str(chr(counter));
+     pressure += currPress;
+     #ser.write(str(chr(counter))) # Convert the decimal number to ASCII then send it to the Arduino
+     #print(ser.readline()) # Read the newest output from the Arduino
+     sleep(.1) # Delay for one tenth of a second
+     if counter == 255:
+         counter = 32
+  
+#voltage = [0.98, 1.15,1.29, 1.51,1.65,1.79,1.98,2.12,2.28,2.45,2.6,2.76,2.93,3.04,3.22,3.34,3.58,3.73,3.9,4.04,4.2,4.37,4.54,4.7,4.92 ]
 
-data = pd.read_csv("testData.csv") ## note: pd.read_csv is how we'll read csv files
+#pressure = [0,40,80,120,160,200,240,280,320,360,400,440,480,520,560,600,640,680,720,760,800,840,880,920,980]
+ 
+plt.plot(voltage, pressure, linewidth = 3,
+         marker='o', markerfacecolor='black', markersize=8)
+  
+# X axis
+plt.xlabel('Voltage')
 
-t = 0
+# Y axis
+plt.ylabel('Pressure')
+  
+plt.title('Voltage v. Pressure test from ARES PT')
+  
+plt.show()
 
-while (t != 538):    
-    data = {'Velocity': data,
-            'Time': t}
-    t += 1
-
-df = pd.DataFrame(data,columns=['Velocity','Time'])
-
-print (df)
