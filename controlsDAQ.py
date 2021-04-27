266d57e3019c7feeb6d2290228a662ed2605a289
Shilpa Rao <shilpa.padiki.rao@gmail.com>
2021-04-27 09:58:47 -0700

    Update controlsDAQ.py

--- controlsDAQ.py
+++ controlsDAQ.py
@@ -1,40 +1,60 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from time import sleep
 import serial
 from serial import Serial
 
-#ser = serial.Serial(port='/dev/cu.usbmodem144301', baudrate=9600)
+# The code below creates a live plot that plots in front of us.
+# Adjust as needed: x and y axes (voltage up to 600, ec)
+                  # pressure data
+                  # currently, voltage = dummy data from arduino
 
 voltage = []
 pressure = []
-i = 0
+##i = 0
 
-#This code goes through 5 iterations, then creates a graph. Takes in random data from a plain arduino uno, just plugged into a computer, and reads lines in. Plots that dummy data on x axis, and 1s on y axis.
-ser = serial.Serial("/dev/cu.usbmodem144301", 9600)
-while (i < 5):
-     cc=str(ser.readline())
-     voltage += [cc[2:][:-5]]
-     pressure += [1]
-     print("I am working")
-     plt.plot(voltage, pressure, linewidth = 3,
-         marker='o', markerfacecolor='black', markersize=8)
-     i += 1
-     
-   #  print(cc[2:][:-5])
+plt.axis([0, 20, 0, 10])
 
-#plt.plot(voltage, pressure, linewidth = 3,
-        # marker='o', markerfacecolor='black', markersize=8)
-  
-# X axis
-plt.xlabel('Voltage')
+ser = serial.Serial("/dev/cu.usbmodem141301", 9600)
+while True:
+    cc=str(ser.readline())
+    voltage += [cc[2:][:-5]]
+    pressure += [np.random.random()]
+    plt.plot(voltage, pressure, linewidth = 1,
+         marker='o', markerfacecolor='black', markersize=2)
+    plt.pause(0.05)
 
-# Y axis
-plt.ylabel('Pressure')
-  
-plt.title('Voltage v. Pressure test from ARES PT')
-  
 plt.show()
 
-# TO DO:
-# What should the terminating condition be for the while loop? How can I get it to update the graph on live feed in front of us?
-# Data backups: how can we save the data in front of us?
+
+# The code below creates a stagnant plot that only shows up after the arduino is done collecting data.
+
+##ser = serial.Serial("/dev/cu.usbmodem141301", 9600)
+##while (i < 5):
+##     cc=str(ser.readline())
+##     voltage += [cc[2:][:-5]]
+##     pressure += [1]
+##     print("I am working")
+##     plt.plot(voltage, pressure, linewidth = 3,
+##         marker='o', markerfacecolor='black', markersize=8)
+##     i += 1
+##    # plt.show()
+##     
+##   #  print(cc[2:][:-5])
+##
+###plt.plot(voltage, pressure, linewidth = 3,
+##        # marker='o', markerfacecolor='black', markersize=8)
+##  
+### X axis
+##plt.xlabel('Voltage')
+##
+### Y axis
+##plt.ylabel('Pressure')
+##  
+##plt.title('Voltage v. Pressure test from ARES PT')
+##  
+##plt.show()
+
+
+#terminating condition for while loop
+#update plot as we go (live)
